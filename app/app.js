87b377c249bdf0d754623f5cdb343ca4d4503979
stefannytoror <stefannyrami@gmail.com>
2019-09-07 10:40:41 -0500

    Add new endpoint

--- app/app.js
+++ app/app.js
@@ -14,4 +14,8 @@ app.get('/guess/:number', function (req, res){
   res.send({result: CodeBreaker.guess(number)});
 });
 
+app.get('/upgrade/', function (req, res){
+  res.send({message: 'Upgrade'});
+});
+
 module.exports = app;
