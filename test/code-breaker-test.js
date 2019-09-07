50f0fbe7ff7a95e153ed199002bf0aae3c05c7b2
stefannytoror <stefannyrami@gmail.com>
2019-09-07 09:13:24 -0500

    Fix test

--- test/code-breaker-test.js
+++ test/code-breaker-test.js
@@ -8,7 +8,7 @@ beforeEach(function() {
 describe('CodeBreaker', function() {
   it('should return XXXX if 1234 is sended', function() {
     let result = CodeBreaker.guess(1234);
-    assert.equal('XXX',result);
+    assert.equal('XXXX',result);
   });
   it('should return EMPTY if 5678 is sended', function() {
     let result = CodeBreaker.guess(5678);
